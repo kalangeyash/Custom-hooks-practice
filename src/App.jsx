bbea08494c01da6be4cb40423ca2f1d77f66c9ec
kalangeyash <yashkalange127@gmail.com>
2024-07-27 20:08:49 +0530

    implemented mouse pointer coordinates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import {useIsOnline} from './useIsOnline'
+import {useIsOnline} from './hooks/useIsOnline'
+import {useMousePointer} from './hooks/useMousePointer'
 function App() {
 
   const isOnline = useIsOnline()
 
-  if(isOnline){
-    return "youre onlline"
-  }
-  if(!isOnline){return "yoi are offfline.,..."}
-  return (
+  // if(isOnline){
+  //   return "youre onlline"
+  // }
+  // if(!isOnline){return "yoi are offfline.,..."}
+
+
+  // mouse pointer
+
+  const mousePointer = useMousePointer()
+
+  
+  return(
     <>
-     
+        your mouse is at position {mousePointer.x} , { mousePointer.y} 
     </>
-  )
+    )
+  
 }
 
 export default App
